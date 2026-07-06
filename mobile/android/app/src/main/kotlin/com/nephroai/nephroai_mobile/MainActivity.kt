44d85561c8acb8db72ac537ee100798f8ce2a065
JolyPab <pablik.pico@gmail.com>
2026-07-06 16:34:05 +0300

    refactor: remove legacy Flutter mobile app

--- mobile/android/app/src/main/kotlin/com/nephroai/nephroai_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.nephroai.nephroai_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- mobile/lib/core/constants.dart
+++
@@ -1,3 +0,0 @@
-const String kApiBaseUrl = 'http://209.50.54.90/api';
-const String kTokenKey = 'jwt_token';
-const String kUserKey = 'user_data';
